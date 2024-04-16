6eac58779e6ffcd8bd4e4cbe53bc6c43eebf6f57
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-16 16:34:20 +0530

    Product model

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { type } = require("os");
 
 app.use(express.json());
 app.use(cors());
@@ -28,6 +29,54 @@ const storage = multer.diskStorage({
       null,
       `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
     );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating upload Endpoint for images
+app.use("/images", express.static("upload/images"));
+
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+// Schema for Creating Products
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
   },
 });
 
