3ff6656f051ef15b784fb562dbfe574d040cf8c3
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-16 14:42:01 +0530

    Multer configuration

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,16 @@ app.get("/", (req, res, next) => {
   res.send("Express App is Running");
 });
 
-
+// Image Storage Engine
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
 app.listen(port, (error) => {
   if (!error) {
