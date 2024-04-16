86f2614674d89d6502b53eae93ddae18b5fbadd4
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-16 14:38:34 +0530

    Backend server creation

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database connection with MongoDB
+mongoose.connect(
+  "mongodb+srv://ParmodKumar:ELVPVlZ91P6Jt4MO@chatterup.c6jztom.mongodb.net/E-Commerce?retryWrites=true&w=majority"
+);
+
+// API creation
+app.get("/", (req, res, next) => {
+  res.send("Express App is Running");
+});
+
+
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running on PORT " + port);
+  } else {
+    console.log("Error : " + error);
+  }
+});
