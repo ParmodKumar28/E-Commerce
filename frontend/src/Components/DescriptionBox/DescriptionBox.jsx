8fcf175e64a89c5d7fddf62db1e51f4a0036bcc3
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-16 14:15:30 +0530

    frontend

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./DescriptionBox.css"
+
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+
+            <div className="descriptionbox-description">
+                <p>E-commerce websites are online platforms that facilitate the buying and selling of goods and services over the internet. They have revolutionized the traditional method of commerce by offering a faster, more convenient way to exchange goods and services. E-commerce websites can cater to a wide range of businesses, from consumer-based retail sites like Amazon.com, auction sites like eBay, music and multimedia sites like Spotify and Netflix, to business exchanges trading goods or services between corporations. These websites are equipped with digital shopping carts, secure payment gateways, and often offer a plethora of products, allowing consumers to browse and shop at their convenience. They also leverage data to provide personalized shopping experiences, recommend products, and promote deals. As technology continues to evolve, e-commerce websites are becoming more sophisticated, providing a seamless and enjoyable shopping experience for consumers worldwide.</p>
+                <p>E-commerce websites are online platforms that facilitate the buying and selling of goods and services over the internet. They have revolutionized the traditional method of commerce by offering a faster, more convenient way to exchange goods and services. E-commerce websites can cater to a wide range of businesses, from consumer-based retail sites like Amazon.com, auction sites like eBay, music and multimedia sites like Spotify and Netflix, to business exchanges trading goods or services between corporations. These websites are equipped with digital shopping carts, secure payment gateways, and often offer a plethora of products, allowing consumers to browse and shop at their convenience. They also leverage data to provide personalized shopping experiences, recommend products, and promote deals. As technology continues to evolve, e-commerce websites are becoming more sophisticated, providing a seamless and enjoyable shopping experience for consumers worldwide.</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
---
+++ frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,69 @@
+// Import's
+import React, { createContext, useState } from "react";
+import all_product from "../Components/Assets/all_product.js"
+
+// Creating Shop context here
+export const ShopContext = createContext(null);
+
+// Function to get default cart product key value pairs like product id with added into cart
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 0; index < all_product.length + 1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+
+// Context provider
+const ShopContextProvider = (props) => {
+    // State's
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    // Function to add a item in a cart by it's id
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        console.log(cartItems);
+    }
+
+    // Function to remove item from a cart by it's id
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    // Function to get total amount in cart
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item));
+                console.log(itemInfo);
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
+
+    // Function to get total item's in cart 
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    // Context value to provide to consumer's
+    const contextValue = { all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems }
+
+    // Returning JSX here providing shop context value to all children component's
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+// Exporting ShopContextProvider component
+export default ShopContextProvider;
