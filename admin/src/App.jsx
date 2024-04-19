64b8dc94189dee66fec90b8d7007d09724294f04
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-19 12:59:45 +0530

    Admin panel add product page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
 
+// Parent App Functional Component Rendring Children
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Admin />
     </div>
   )
 }
