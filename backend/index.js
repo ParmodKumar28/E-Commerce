35ca9104688c7f10b1cca96ccf1f1d3f55dee902
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-22 13:25:56 +0530

    Add Remove And Get All Cart Items Feature

--- backend/index.js
+++ backend/index.js
@@ -219,6 +219,57 @@ app.get("/popularinwomen", async (req, res) => {
   res.send(popular_in_women);
 });
 
+// Creating middleware to fetch user
+const fetchUser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using valid token" });
+  } else {
+    try {
+      const data = jwt.verify(token, "secret_ecom");
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res
+        .status(401)
+        .send({ errors: "please authenticate using a valid token" });
+    }
+  }
+};
+
+// Creating endpoint for adding products in cartdata
+app.post("/addtocart", fetchUser, async (req, res) => {
+  console.log("Added", req.body.itemId);
+  let userData = await Users.findOne({ _id: req.user.id });
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Added");
+});
+
+// Creating endpoint to remove product from cartData
+app.post("/removefromcart", fetchUser, async (req, res) => {
+  console.log("removed", req.body.itemId);
+  let userData = await Users.findOne({ _id: req.user.id });
+  if (userData.cartData[req.body.itemId] > 0) {
+    userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate(
+      { _id: req.user.id },
+      { cartData: userData.cartData }
+    );
+    res.send("Removed");
+  }
+});
+
+// Creating ednpoint for get cartdata
+app.post("/getcart", fetchUser, async (req, res) => {
+  console.log("Get cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  res.json(userData.cartData);
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server Running on PORT " + port);
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -24,17 +24,55 @@ const ShopContextProvider = (props) => {
         fetch('http://localhost:4000/allproducts')
             .then((response) => response.json())
             .then((data) => setAll_Product(data))
+
+        if (localStorage.getItem('auth-token')) {
+            fetch('http://localhost:4000/getcart', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/form-data',
+                    'auth-token': `${localStorage.getItem('auth-token')}`,
+                    'Content-Type': 'application/json',
+                },
+                body: "",
+            }).then((response) => response.json())
+                .then((data) => setCartItems(data));
+        }
     }, []);
 
     // Function to add a item in a cart by it's id
     const addToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
-        console.log(cartItems);
+        if (localStorage.getItem('auth-token')) {
+            fetch('http://localhost:4000/addtocart', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/form-data',
+                    'auth-token': `${localStorage.getItem('auth-token')}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "itemId": itemId })
+            })
+                .then((response) => response.json())
+                .then((data) => console.log(data))
+        }
     }
 
     // Function to remove item from a cart by it's id
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+        if (localStorage.getItem('auth-token')) {
+            fetch('http://localhost:4000/removefromcart', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/form-data',
+                    'auth-token': `${localStorage.getItem('auth-token')}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "itemId": itemId })
+            })
+                .then((response) => response.json())
+                .then((data) => console.log(data))
+        }
     }
 
     // Function to get total amount in cart
